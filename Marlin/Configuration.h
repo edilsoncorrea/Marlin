d50f5caa303b3f564ed65eb9342715ed7239c4d4
edilsoncorrea <edilsoncorrea117@gmail.com>
2022-08-09 21:57:29 -0300

    Ajuste da margem esquerda do eixo X devido à troca do endstop para ótico.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1699,7 +1699,7 @@
 #define Y_BED_SIZE 246
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -4
+#define X_MIN_POS -7
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
